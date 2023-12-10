751cec7b6df356546705e7bc5d907ef240506031
Saad Altaf <Saad Altaf@DESKTOP-9HOJDD5>
2023-12-10 14:45:23 +0500

    setup of initial structure

--- src/ApiCalls.tsx
+++ src/ApiCalls.tsx
@@ -1,3 +1,32 @@
-export function callApi() {
-    alert('test');
-}
+import axios from 'axios';
+var baseUrl = "http://localhost:5279/"
+
+
+export const getAllPractitioners = async () => {
+    try {
+        const response = await axios.get(baseUrl+'practitioners');
+        return response.data;
+    } catch (error) {
+        return 'some error occured ' + error;
+    }
+
+};
+
+
+export const getSupervisors = async () => {
+    try {
+        const response = await axios.get(baseUrl+'practitioners/supervisors');
+        return response.data;
+    } catch (error) {
+        return 'some error occured ' + error;
+    }
+};
+
+export const getRemaining = async () => {
+    try {
+        const response = await axios.get(baseUrl+'practitioners/remaining');
+        return response.data;
+    } catch (error) {
+        return 'some error occured ' + error;
+    }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./app.css";
-import { callApi } from "./ApiCalls";
+import { getRemaining, getSupervisors } from "./ApiCalls";
 
 function App() {
 
-  useEffect(() => {
-    callApi()
-  }, [])
-  
+  const [supervisors, setSupervisors] = useState([])
+  const [remaining, setRemaining] = useState([])
 
+  useEffect(() => {
+    getSupervisors().then((payload) => {
+      setSupervisors(payload)
+    });
+    getRemaining().then((payload) => {
+      setRemaining(payload)
+    });
+
+  }, [])
 
   return (
     <div className="h-screen w-full appshell">
       <div className="header flex flex-row items-center p-2 bg-primary shadow-sm">
         <p className="font-bold text-lg">coreplus</p>
       </div>
-      <div className="supervisors">Supervisor practitioners</div>
-      <div className="praclist">Remaining Practitioners</div>
-      <div className="pracinfo">Practitioner Report UI</div>
+      <div className="supervisors">
+        <p className="underline">Supervisor practitioners</p>
+        <div className="w-full ">
+          {supervisors && supervisors.map((item) => (
+            <p className="hover:bg-yellow-700 p-1 rounded cursor-pointer">{item.name}</p>
+          ))}
+        </div>
+      </div>
+      <div className="praclist">
+        <p className="underline">Remaining Practitioners</p>
+        <div className="w-full ">
+          {remaining && remaining.map((item) => (
+            <p className="hover:bg-yellow-700 p-1 rounded cursor-pointer">{item.name}</p>
+          ))}
+        </div>
+      </div>
+      <div className="pracinfo">
+        <div className="w-full flex items-center justify-between">
+          <p>
+            Practitioner Report UI
+          </p>
+          <input type="date"></input>
+        </div>
+
+      </div>
     </div>
   );
 }
